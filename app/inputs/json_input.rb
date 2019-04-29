d7c597fe0f07780423f3d7f3c08a877ecb083069
Maximiliano Guzman <maximiliano.guzman@gmail.com>
2019-04-29 20:50:45 -0300

    now supports default values

--- app/inputs/json_input.rb
+++ app/inputs/json_input.rb
@@ -15,9 +15,17 @@ class JsonInput < Formtastic::Inputs::TextInput
 
   def to_html
     html = '<div class="jsoneditor-wrap">'
-    current_value = @object.public_send method
+    value = @object.public_send method
+    value = options[:value] if options[:value]
+    
+    if value.respond_to?(:to_json)
+      value_as_json = value.to_json
+    else
+      value
+    end
+
     html << builder.text_area(method, input_html_options.merge(
-                                        value: (current_value.respond_to?(:to_json) ? current_value.to_json : ''), 'data-options': options[:editor_options].to_json ))
+                                        value: value_as_json, 'data-options': options[:editor_options].to_json ))
     html << '</div>'
     html << '<div style="clear: both"></div>'
     input_wrapping do
